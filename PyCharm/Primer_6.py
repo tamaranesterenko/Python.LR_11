2e6b31bf4f282571a850b9713162e89d6df3d154
tamaranesterenko <90053368+tamaranesterenko@users.noreply.github.com>
2021-12-15 22:30:40 +0300

    Update Primer_6.py

--- PyCharm/Primer_6.py
+++ PyCharm/Primer_6.py
@@ -1,22 +1,25 @@
 import math
 import sys
 
-
 figure = input("1-прямоугольник, 2-треугольник, 3-круг: ")
+
 
 def rectangle():
     a = float(input("Ширина: "))
     b = float(input("Высота: "))
     print(f"Площадь: {a * b}")
 
+
 def triangle():
     a = float(input("Основание: "))
     h = float(input("Высота: "))
     print(f"Площадь: {0.5 * a * h}")
 
+
 def circle():
     r = float(input("Радиус: "))
-    print(f"Площадь: {math.pi * r**2}")
+    print(f"Площадь: {math.pi * r ** 2}")
+
 
 if figure == '1':
     rectangle()
