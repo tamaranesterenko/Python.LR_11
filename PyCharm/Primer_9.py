bc220883b45bebcb17a604834694a0d93e9278ed
tamaranesterenko <90053368+tamaranesterenko@users.noreply.github.com>
2021-12-15 22:32:45 +0300

    Update Primer_9.py

--- PyCharm/Primer_9.py
+++ PyCharm/Primer_9.py
@@ -5,9 +5,10 @@ def cylinder():
     r = float(input())
     h = float(input())
     side = 2 * math.pi * r * h
-    circle = math.pi * r**2
+    circle = math.pi * r ** 2
     full = side + 2 * circle
     return full
+
 
 square = cylinder()
 print(square)
